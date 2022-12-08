6267e333625b686c503867b382d68010b1314f01
DainiusMI <mental.def@gmail.com>
2022-12-08 22:44:07 +0200

    basic design

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,42 @@ import { useState } from 'react'
 import Die from "./Die"
 import './App.css'
 
+
 export default function App() {
 
+  const [diceArr, setDiceArr] = useState(rollDice());
+  const [gameState, setGameState] = useState("Roll")
+
+  function rollDice() {
+    const result = []
+
+    for (let i = 0; i < 10; i++) {
+      result.push({
+        id: i,
+        value: Math.floor(Math.random() * 6 + 1 ),
+        isHeld: false
+      })
+    }
+    return result
+  }
 
   return (
     <main className='tenzies'>
       <h1 className='title'>Tenzies</h1>
       <p className='description'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <section className="die-grid">
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
+        {
+          diceArr.map((die, idx) => {
+            return <Die
+              key={idx} 
+              id={idx} 
+              value={die.value}
+              isHeld={die.isHeld}
+            />
+          })
+        }
       </section>
-      <button>Roll</button>
+      <button onClick={()=>{setDiceArr(rollDice())}}>{gameState}</button>
     </main>
   )
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 export default function GameContainer(props) {
+    console.log(props.isHeld)
     return (
-        <div className="die">{props.value}</div>
+        <div 
+            className="die"
+            style={{backgroundColor: props.isHeld ? "#59E391" : "#FFF"}}
+        >{props.value}</div>
     )
 }
